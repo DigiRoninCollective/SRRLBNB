af017746cb83b00ba6a7c8549984aaf543866c8c
DigiRoninCollective <ra12audi@gmail.com>
2025-10-22 01:21:29 -0500

    fix: Use margin-top instead of padding-top to properly position hero below fixed navbar

--- src/App.jsx
+++ src/App.jsx
@@ -90,6 +90,7 @@ function App() {
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             transition={{ duration: 1 }}
+            style={{ paddingTop: '0' }}
           >
             {/* Navbar */}
             <motion.nav
